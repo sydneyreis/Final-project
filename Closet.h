2c4e1461201763d0d0f2a100a4cda9f4692a0ded
lavendera03 <alicia.lavender3@gmail.com>
2026-04-03 12:29:01 -0400

    Code is Runninggit add Makefile

--- Closet.h
+++ Closet.h
@@ -1,73 +1,87 @@
 #ifndef CLOSET_H
 #define CLOSET_H
 
-#include "Clothing.h"
 #include <iostream>
-#include <string>
 #include <vector>
-#include <map>
+#include <string>
+#include "Clothing.h"
+
+using namespace std;
 
 template <typename T>
-class Closet{
-    public:
-        //void addClothing(string itemType, vector<T> itemAttributes);
-        Closet();
-        void addClothing();
-        void removeItem(string name);
-        void showCloset();
-        vector<Clothing*> generateOutfit(int dressiness, int avgTemp);
-        void saveOutfit(vector<Clothing*> newOutfit);
-        void showOutfits();
-        ~Closet();
-        vector<vector<Clothing*>> savedOutfits;
-        vector <Clothing*> closetItems;
+class Closet {
+public:
+    vector<Clothing*> closetItems;
+    vector<vector<Clothing*>> savedOutfits;
+
+    // Constructor
+    Closet();
+
+    // Core features
+    void addClothing();
+    void removeItem(string name);
+    void showCloset();
+    void saveOutfit(vector<Clothing*> newOutfit);
+    void showOutfits();
+    vector<Clothing*> generateOutfit(int dressiness, int avgTemp);
 };
 
-// ----------------------- Constructor -----------------------
-template <typename T>
-Closet<T>::Closet(){}
 
-// ----------------------- Methods -----------------------
+// ================= IMPLEMENTATION =================
+
+// Constructor
+template <typename T>
+Closet<T>::Closet() {}
+
+
+// -------------------- SHOW CLOSET --------------------
 template <typename T>
 void Closet<T>::showCloset() {
-/* Shows the item name and description for each item in the user's closet */
     cout << "----- CLOSET -----" << endl;
+
     if (closetItems.empty()) {
         cout << "(empty)" << endl;
     } else {
-        for (int item = 0; item < closetItems.size(); item++) {
-            cout << "+ " << closetItems[item]->getName() << ": " << closetItems[item]->itemDescription() << endl;
+        for (int i = 0; i < closetItems.size(); i++) {
+            cout << "+ " << closetItems[i]->getName()
+                 << ": " << closetItems[i]->itemDescription() << endl;
         }
-    } cout << "---------------------" << endl;
-    cout << endl;
+    }
+
+    cout << "---------------------\n" << endl;
 }
 
+
+// -------------------- SAVE OUTFIT --------------------
 template <typename T>
 void Closet<T>::saveOutfit(vector<Clothing*> newOutfit) {
-/* Stores a group of clothing items that pair well together 
-        - savedOutfits: The vector containg all of the current saved outfits
-    */
     savedOutfits.push_back(newOutfit);
 }
 
+
+// -------------------- SHOW OUTFITS --------------------
 template <typename T>
 void Closet<T>::showOutfits() {
-/* Outputs all of the outfits that the user has saved */
     cout << "----- SAVED OUTFITS -----" << endl;
+
     if (savedOutfits.empty()) {
         cout << "(No saved outfits)" << endl;
     } else {
-        for (int outfit = 0; outfit < savedOutfits.size(); outfit++) {
-            cout << "Outfit " << outfit << endl;
-            for (int item = 0; item < savedOutfits[outfit].size(); item++) {
-                cout << "+ " << savedOutfits[outfit][item]->getName() << ": " << savedOutfits[outfit][item]->itemDescription() << endl;
-            }       
+        for (int i = 0; i < savedOutfits.size(); i++) {
+            cout << "Outfit " << i << endl;
+
+            for (int j = 0; j < savedOutfits[i].size(); j++) {
+                cout << "+ " << savedOutfits[i][j]->getName()
+                     << ": " << savedOutfits[i][j]->itemDescription() << endl;
+            }
         }
-    } cout << "---------------------" << endl;
-    cout << endl;
+    }
+
+    cout << "---------------------\n" << endl;
 }
 
-// ----------------------- Methods for adding clothing items to closets -----------------------
+
+// -------------------- ADD CLOTHING --------------------
 template <typename T>
 void Closet<T>::addClothing() {
 
@@ -83,219 +97,140 @@ void Closet<T>::addClothing() {
     string color;
     getline(cin, color);
 
-    cout << "Enter yes if your item is patterned if not enter no" << endl;
+    cout << "Enter yes if patterned, otherwise no" << endl;
     string patt;
     getline(cin, patt);
-    bool patty = false;
-    if (patt == "yes"){
-        patty = true;
-    }
+    bool patty = (patt == "yes");
 
-    cout << "On a scale 0-5 how warm is your item" << endl;
-    cout << "With 5 being not warm at all and 0 being very warm" << endl;
+    cout << "Warmth level (0–5): " << endl;
     int warmth;
     cin >> warmth;
     cin.ignore();
 
-
-    cout << "Enter yes if you would wear this item in the rain" << endl;
+    cout << "Wear in rain? (yes/no)" << endl;
     string rain;
     getline(cin, rain);
-    bool rainy = false;
-    if (rain == "yes"){
-        rainy = true;
-    }
+    bool rainy = (rain == "yes");
 
-    cout << "Enter yes if you would consider this item to be a basic" << endl;
+    cout << "Is it a basic? (yes/no)" << endl;
     string basic;
     getline(cin, basic);
-    bool bassic = false;
-    if (basic == "yes"){
-        bassic = true;
-    }
+    bool bassic = (basic == "yes");
 
-    cout << "On a scale 1-5 how dressy is your item: " << endl;
+    cout << "Dressiness (1–5): " << endl;
     int dressy;
     cin >> dressy;
     cin.ignore();
 
 
-    if (type == "Top" ) { 
-        cout << "Is your top 1 long sleeve, 2 short sleeve, or 3 sleevless. Enter a number: " << endl;
-        int length;
-        cin >> length;
-        cin.ignore();
-        closetItems.push_back(new Top(name, type, color, patty, warmth, rainy, bassic, dressy, to_string(length)));
+    // -------- TYPE HANDLING --------
+    if (type == "Top") {
+        cout << "Sleeve length (long/short/sleeveless): " << endl;
+        string length;
+        getline(cin, length);
+
+        closetItems.push_back(
+            new Top(name, type, color, patty, warmth, rainy, bassic, dressy, length)
+        );
     }
-    
-     else if (type == "Bottom"){
-        cout << "Enter yes if this item is shorts" << endl;
-        string isShort;
+
+    else if (type == "Bottom") {
+        string isShort, isSkirt;
+        cout << "Shorts? (yes/no): ";
         getline(cin, isShort);
-        bool shorty = false;
-        if (isShort == "yes"){
-            shorty = true;
-        }
-        cout << "Enter yes if this item is a skirt" << endl;
-        string skirt;
-        getline(cin, skirt);
-        bool skirty = false;
-        if (skirt == "yes"){
-            skirty = true;
-        }
-        closetItems.push_back(new Bottom(name, type, color, patty, warmth, rainy, bassic, dressy, shorty, skirty));
+        cout << "Skirt? (yes/no): ";
+        getline(cin, isSkirt);
+
+        closetItems.push_back(
+            new Bottom(name, type, color, patty, warmth, rainy, bassic, dressy,
+                       isShort == "yes", isSkirt == "yes")
+        );
     }
-    
 
     else if (type == "Dress") {
-        cout << "How long is your dress" << endl;
         string length;
+        cout << "Dress length: ";
         getline(cin, length);
-        closetItems.push_back(new Dress(name, type, color, patty, warmth, rainy, bassic, dressy, length));
+
+        closetItems.push_back(
+            new Dress(name, type, color, patty, warmth, rainy, bassic, dressy, length)
+        );
     }
-    
 
-     else if (type == "Accessories") { 
-        cout << "What type of accesory is this " << endl;
-        string typpe;
-        getline(cin, typpe);
-        closetItems.push_back(new Accessories(name, type, color, patty, warmth, rainy, bassic, dressy, typpe));
-    } 
-    
-    
-    else if (type == "Coat") { 
-        string length;
-        cout << "Enter coat length: ";
+    else if (type == "Coat") {
+        string length, wp;
+        cout << "Coat length: ";
         getline(cin, length);
-
-        bool waterproof;
         cout << "Waterproof? (yes/no): ";
-        string wp;
         getline(cin, wp);
-        waterproof = (wp == "yes");
-        closetItems.push_back(new Coat(name, type, color, patty, warmth, rainy, bassic, dressy, length, waterproof));
-    }  
-    
-    
-    else if (type == "Shoes") { 
-        cout << "Enter yes if these shoes are open toe" << endl;
-        string open;
-        getline(cin, open);
-        bool openToe = false;
-        if (open == "yes"){
-            openToe = true;
-        }
 
-        cout << "Enter yes if these shoes are heels" << endl;
-        string heel;
-        getline(cin, heel);
-        bool heely = false;
-        if (heel == "yes"){
-            heely = true;
-        }
-
-        cout << "Enter yes if these shoes ar athletic sneakers" << endl;
-        string sneak;
-        getline(cin, sneak);
-        bool sneaky = false;
-        if (sneak == "yes"){
-            sneaky = true;
-        }
-        closetItems.push_back(new Shoes(name, type, color, patty, warmth, rainy, bassic, dressy, openToe, heely, sneaky));
+        closetItems.push_back(
+            new Coat(name, type, color, patty, warmth, rainy, bassic, dressy,
+                     length, wp == "yes")
+        );
     }
 
+    else if (type == "Shoes") {
+        string open, heel, sneak;
 
+        cout << "Open toe? (yes/no): ";
+        getline(cin, open);
+        cout << "Heels? (yes/no): ";
+        getline(cin, heel);
+        cout << "Sneakers? (yes/no): ";
+        getline(cin, sneak);
+
+        closetItems.push_back(
+            new Shoes(name, type, color, patty, warmth, rainy, bassic, dressy,
+                      open == "yes", heel == "yes", sneak == "yes")
+        );
+    }
+
+    else if (type == "Accessories") {
+        string typpe;
+        cout << "Accessory type: ";
+        getline(cin, typpe);
+
+        closetItems.push_back(
+            new Accessories(name, type, color, patty, warmth, rainy, bassic, dressy, typpe)
+        );
+    }
 }
 
+
+// -------------------- REMOVE ITEM --------------------
 template <typename T>
 void Closet<T>::removeItem(string name) {
-    bool exists = false;
-    int index = -1;
-    for(int i = 0; i < closetItems.size(); i++ ){
-        if(closetItems[i]->getName() == name){
-            exists = true;
-            index = i;
-            break;
+    for (int i = 0; i < closetItems.size(); i++) {
+        if (closetItems[i]->getName() == name) {
+            closetItems.erase(closetItems.begin() + i);
+            return;
         }
     }
-    if(exists){
-        closetItems.erase(closetItems.begin() + index);
-    }
-    else{
-        cout << "An item with that name was not found in your closet";
-    }
 
+    cout << "Item not found." << endl;
 }
 
+
+// -------------------- GENERATE OUTFIT --------------------
 template <typename T>
 vector<Clothing*> Closet<T>::generateOutfit(int dressiness, int avgTemp) {
 
     vector<Clothing*> outfit;
-    vector<Clothing*> tops;
-    vector<Clothing*> bottoms;
-    vector<Clothing*> shoes;
+    vector<Clothing*> tops, bottoms, shoes;
 
-    for(int i =0; i < closetItems.size(); i ++){
-        if(closetItems[i]->getType() == "Top"){
-            tops.push_back(closetItems[i]);
-        }
-        if(closetItems[i]->getType() == "Bottom"){
-            bottoms.push_back(closetItems[i]);
-        }
-        if(closetItems[i]->getType() == "Shoes"){
-            shoes.push_back(closetItems[i]);
-        }
+    for (int i = 0; i < closetItems.size(); i++) {
+        if (closetItems[i]->getType() == "Top") tops.push_back(closetItems[i]);
+        if (closetItems[i]->getType() == "Bottom") bottoms.push_back(closetItems[i]);
+        if (closetItems[i]->getType() == "Shoes") shoes.push_back(closetItems[i]);
     }
 
-    int minDress = dressiness - 1;
-    int maxDress = dressiness + 1;
-
-    //removing items that are not the appropriate level of dressy
-    for(int i = tops.size() - 1; i >= 0; i--){
-        if (!(tops[i]->getDressiness() >= minDress && tops[i]->getDressiness() <= maxDress)) {
-            tops.erase(tops.begin() + i);
-        }
-    }
-
-    for(int i = bottoms.size() - 1; i >= 0; i--){
-        if(!(bottoms[i]->getDressiness() >= minDress && bottoms[i]->getDressiness() <= maxDress)){
-            bottoms.erase(bottoms.begin() + i);
-        }
-    }
-
-    for(int i = shoes.size() - 1; i >= 0; i--){
-        if(!(shoes[i]->getDressiness() >= minDress && shoes[i]->getDressiness() <= maxDress)){
-            shoes.erase(shoes.begin() + i);
-        }
-    }
-
-    //removing inappropriate weather items 
-
-    for(int i = tops.size() - 1; i >= 0; i--){
-        int warmth = tops[i]->getWarmth() * 15;
-        if(!(warmth >= avgTemp - 10 && warmth <= avgTemp + 10)){
-            tops.erase(tops.begin() + i);
-        }
-    }
-
-    for(int i = bottoms.size() - 1; i >= 0; i--){
-        int warmth = bottoms[i]->getWarmth() * 15;
-        if(!(warmth >= avgTemp - 10 && warmth <= avgTemp + 10)){
-            bottoms.erase(bottoms.begin() + i);
-        }
-    }
-
-    for(int i = shoes.size() - 1; i >= 0; i--){
-        int warmth = shoes[i]->getWarmth() * 15;
-        if(!(warmth >= avgTemp - 10 && warmth <= avgTemp + 10)){
-            shoes.erase(shoes.begin() + i);
-        }
-    }
-
-    int top = tops.size();
-    int bottom = bottoms.size();
-    int shoe = shoes.size();
+    // (simplified for now)
+    if (!tops.empty()) outfit.push_back(tops[0]);
+    if (!bottoms.empty()) outfit.push_back(bottoms[0]);
+    if (!shoes.empty()) outfit.push_back(shoes[0]);
 
     return outfit;
 }
+
 #endif
